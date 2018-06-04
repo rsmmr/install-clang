fc1ec00422b86121f0c99862aac196c1d278806b
Robin Sommer <robin@joker.corelight>
2018-06-04 14:46:18 +0000

    Updates for clang 6.0
    Tested on Linux only so far.

--- test.cc
+++ test.cc
@@ -1,9 +1,15 @@
 
 #include <algorithm>
+#include <experimental/filesystem>
 #include <iostream>
+#include <optional>
 
 int main(int argc, char** argv)
 {
+    std::optional opt = std::make_optional<std::string>("optional string");
+
     std::cout << "Hello, Clang!" << std::endl;
+    std::cout << "have_fs=" << std::experimental::filesystem::exists("/etc") << std::endl;
+    std::cout << "optional=" << *opt << std::endl;
     return 1;
 }
